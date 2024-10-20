fb3991148f6194723493fa77ec2d122dbe5c8a9d
skyu93 <seungkyu0215@gmail.com>
2024-10-20 21:50:27 +0900

    reactivity (reactive, ref, computed, watch) 구현

--- src/reactivity.ts
+++ src/reactivity.ts
@@ -1,6 +1,9 @@
+import { cloneDeep, isEqual, isFunction, isNil, isNotNil, isObject } from './shared';
+
 type Effect = () => void;
 type Dep = Set<Effect>;
 type KeyToDepMap = Map<any, Dep>;
+
 const targetMap = new WeakMap<object, KeyToDepMap>();
 let activeEffect: Effect | null = null;
 
@@ -16,7 +19,6 @@ const track = (target: any, key: string) => {
   if (!dep) {
     depsMap.set(key, (dep = new Set()));
   }
-
   dep.add(activeEffect);
 };
 
@@ -27,6 +29,12 @@ const trigger = (target: any, key: string) => {
     ?.forEach((effect) => effect());
 };
 
+const registerEffect = (effect: Effect) => {
+  activeEffect = effect;
+  effect();
+  activeEffect = null;
+};
+
 const reactive = <T extends object>(target: T): T => {
   return new Proxy(target, {
     get(target: T, key: string, receiver: T): any {
@@ -35,11 +43,11 @@ const reactive = <T extends object>(target: T): T => {
     },
     set(target: T, key: string, newValue: T, receiver: T): boolean {
       const oldValue = Reflect.get(target, key, receiver);
-      const setNewValueResult = Reflect.set(target, key, newValue, receiver);
+      const success = Reflect.set(target, key, newValue, receiver);
       if (oldValue !== newValue) {
         trigger(target, key);
       }
-      return setNewValueResult;
+      return success;
     },
   });
 };
@@ -48,7 +56,99 @@ interface RefImpl<T> {
   value: T;
 }
 const ref = <T>(value: T): RefImpl<T> => {
+  if (isObject(value)) {
+    let reactiveValue = reactive(value);
+    return {
+      get value() {
+        return reactiveValue;
+      },
+      set value(newValue: T) {
+        if (isObject(newValue)) {
+          reactiveValue = reactive(newValue);
+        }
+      },
+    };
+  }
   return reactive({ value });
 };
 
-export { reactive, ref };
+type ComputedGetter<T> = () => T;
+type ComputedSetter<T> = (value: T) => void;
+type ComputedProps<T> = ComputedGetter<T> | { get: ComputedGetter<T>; set: ComputedSetter<T> };
+const isEffectGetter = <T>(effect: ComputedProps<T>): effect is ComputedGetter<T> => {
+  return isFunction(effect);
+};
+const computed = <T>(props: ComputedProps<T>) => {
+  let getter: ComputedGetter<T>;
+  let setter: ComputedSetter<T>;
+  let computedRef: T;
+
+  if (isEffectGetter(props)) {
+    getter = props;
+  } else {
+    getter = props.get;
+    setter = props.set;
+  }
+
+  registerEffect(() => {
+    if (isNil(getter)) return;
+
+    const result = getter();
+    if (isNotNil(result) && computedRef !== result) {
+      computedRef = result;
+    }
+  });
+
+  return {
+    get value(): T {
+      return computedRef;
+    },
+    set value(newValue: T) {
+      if (isNotNil(setter)) {
+        setter(newValue);
+      }
+    },
+  };
+};
+
+interface WatchOptions {
+  immediate?: boolean;
+  deep?: boolean;
+}
+const watch = <T>(
+  source: () => T,
+  cb: (value: T, oldValue: T | undefined, onCleanup: (cleanupFn: () => void) => void) => void,
+  options?: WatchOptions
+) => {
+  let { immediate = false, deep = false } = options ?? {};
+  let oldValue: T | undefined;
+  let cleanup: (() => void) | undefined;
+
+  const onCleanup = (fn: () => void) => {
+    cleanup = fn;
+  };
+
+  const runner = () => {
+    if (cleanup) {
+      cleanup();
+      cleanup = undefined;
+    }
+
+    const value = source();
+    if (deep ? !isEqual(value, oldValue) : value !== oldValue) {
+      cb(value, oldValue, onCleanup);
+      oldValue = deep ? cloneDeep(value) : value;
+    }
+  };
+
+  registerEffect(() => {
+    if (immediate) {
+      runner();
+    } else {
+      immediate = true;
+      oldValue = source();
+    }
+  });
+};
+
+export { reactive, ref, computed, watch };
